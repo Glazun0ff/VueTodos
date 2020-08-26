f1ad7d612959814ca2732b680cbed879c47d5e5a
ZeroCool <g0s@ya.ru>
2020-08-26 15:52:24 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Главная</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dignissimos eveniet excepturi expedita impedit numquam quae quas tenetur velit voluptas!</p>
+
+    <router-link to="/todos">Задачи</router-link>
+  </div>
+</template>
